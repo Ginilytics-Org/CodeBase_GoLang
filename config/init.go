299b1ee5d9f20040bc23c8974fa9d72192a6fbe7
MukulRana07 <mukul@ginilytics.com>
2025-05-16 11:57:24 +0530

    inital commit

---
+++ config/init.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"CodeBase_GoLang/app/controller"
+	"CodeBase_GoLang/app/repository"
+	"CodeBase_GoLang/app/service"
+)
+
+type Initialization struct {
+	userRepo repository.UserRepository
+	userSvc  service.UserService
+	UserCtrl controller.UserController
+	RoleRepo repository.RoleRepository
+}
+
+func NewInitialization(userRepo repository.UserRepository,
+	userService service.UserService,
+	userCtrl controller.UserController,
+	roleRepo repository.RoleRepository) *Initialization {
+	return &Initialization{
+		userRepo: userRepo,
+		userSvc:  userService,
+		UserCtrl: userCtrl,
+		RoleRepo: roleRepo,
+	}
+}
